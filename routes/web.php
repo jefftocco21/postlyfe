0c31160d5fcade69c15c4c2d77d308b7b14e5839
jnt369@gmail.com <jeff.tocco@lyonsexpress.com>
2022-05-23 14:35:13 -0400

    Login route has been created

--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,7 @@
 
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\DashController;
+use App\Http\Controllers\Auth\LoginController;
 use App\Http\Controllers\Auth\RegisterController;
 
 /*
@@ -18,9 +19,13 @@ use App\Http\Controllers\Auth\RegisterController;
 
 Route::get('/dash', [DashController::class, 'index'])->name('dash');
 
-Route::get('/posts', function () {
-    return view('posts.index');
-});
+
+Route::get('/login', [LoginController::class, 'index'])->name('login');
+Route::post('/login', [LoginController::class, 'store']);
 
 Route::get('/register', [RegisterController::class, 'index'])->name('register');
 Route::post('/register', [RegisterController::class, 'store']);
+
+Route::get('/posts', function () {
+    return view('posts.index');
+});
